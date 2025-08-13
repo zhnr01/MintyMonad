f86fd0c4818ffec189bec146a0d64c992fcd706a
zhnr01 <zeeshanrabnawaz.se@gmail.com>
2025-08-13 08:52:02 +0500

    Add login_required decorator to enforce user authentication on routes

---
+++ app/decorators.py
@@ -0,0 +1,11 @@
+from functools import wraps
+from flask import session, redirect, url_for, flash
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session:
+            flash('Please log in to access this page.', 'warning')
+            return redirect(url_for('auth.login'))  
+        return f(*args, **kwargs)
+    return decorated_function
