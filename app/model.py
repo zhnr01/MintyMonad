4d9dcffc37872ff6f9ad2fe77e1a50d20317a2c0
zhnr01 <zeeshanrabnawaz.se@gmail.com>
2025-08-14 07:32:47 +0500

    Database model classes

--- app/model.py
+++ app/model.py
@@ -11,3 +11,28 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.wallet_address}>'
+    
+class NFT(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    token_id = db.Column(db.Integer, nullable=False)
+    contract_address = db.Column(db.String(42), nullable=False)
+    name = db.Column(db.String(100))
+    image_url = db.Column(db.String(255))
+    description = db.Column(db.Text)
+    price = db.Column(db.Float, nullable=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    offers = db.relationship('Offer', backref='nft', lazy=True)
+
+    def __repr__(self):
+        return f'<NFT {self.name} #{self.token_id}>'
+
+class Offer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nft_id = db.Column(db.Integer, db.ForeignKey('nft.id'), nullable=False)  # Note lowercase 'nft'
+    buyer_wallet = db.Column(db.String(42), nullable=False)
+    offer_price = db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    status = db.Column(db.String(20), default='pending')
+
+    def __repr__(self):
+        return f'<Offer for NFT {self.nft_id} at {self.offer_price}>'
