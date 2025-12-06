51a1380d74846c858fb7fc9acb754bc0f3f89755
zhnr01 <zeeshanrabnawaz.se@gmail.com>
2025-12-06 19:13:37 +0500

    config: add MONAD_* defaults (chain id/name/native/explorer/gas limit)

--- config.py
+++ config.py
@@ -15,6 +15,13 @@ class Config:
     NFT_MARKETPLACE_CONTRACT_ADDRESS = os.environ.get('NFT_MARKETPLACE_CONTRACT_ADDRESS')
     # Prefer MONAD_RPC_URL, fallback to MONAD_RPC for compatibility
     MONAD_RPC_URL = os.environ.get('MONAD_RPC_URL') or os.environ.get('MONAD_RPC')
+    MONAD_CHAIN_ID = os.environ.get('MONAD_CHAIN_ID', 10143)
+    MONAD_CHAIN_NAME = os.environ.get('MONAD_CHAIN_NAME', 'Monad Testnet')
+    MONAD_NATIVE_NAME = os.environ.get('MONAD_NATIVE_NAME', 'Monad')
+    MONAD_NATIVE_SYMBOL = os.environ.get('MONAD_NATIVE_SYMBOL', 'MON')
+    MONAD_NATIVE_DECIMALS = os.environ.get('MONAD_NATIVE_DECIMALS', 18)
+    MONAD_EXPLORER_URL = os.environ.get('MONAD_EXPLORER_URL', 'https://testnet.monadexplorer.com/')
+    MONAD_BLOCK_GAS_LIMIT = os.environ.get('MONAD_BLOCK_GAS_LIMIT', 150000000)
 
     @staticmethod
     def init_app(app):
